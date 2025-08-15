9efaf6acf3f7c69c564d648cc3893439620127ba
thegreatfudge <gacek.m93@gmail.com>
2025-08-15 16:39:19 +0200

    Initial Commit

---
+++ Source/Autobattler/Autobattler.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Autobattler.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Autobattler, "Autobattler" );
